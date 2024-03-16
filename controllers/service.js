380a97ea0eae7ab45e594c04e01099e31cab6347
Shashank <shashankas0510@gmail.com>
2024-03-16 21:17:24 +0530

    second commit

--- controllers/service.js
+++ controllers/service.js
@@ -1,19 +1,31 @@
+import { instance } from "../index.js"
+import { Payment } from "../models/payment.js"
 import { Service } from "../models/service.js"
 import ApiFeatures from "../utils/apifeatures.js"
+import crypto from 'crypto'
 
 export const createService = async (req, res) => {
     try {
         const { name, description, price, image, category, reviews } = req.body
-        await Service.create({
+
+        const services = {
             name,
             description,
             price,
             image,
             category,
             reviews
-        })
+        }
+
+        const options ={
+            amount:Number(price)*100,
+            currency:"INR"
+        }
+        const order = await instance.orders.create(options)
+        
         res.status(201).json({
             success: true,
+            order,services,
             message: "service added successfully"
         })
     } catch (error) {
@@ -23,6 +35,46 @@ export const createService = async (req, res) => {
         })
     }
 }
+
+//payment verification
+export const paymentverification =  async(req,res,next)=>{
+    try {
+        const {razorpay_order_id, razorpay_payment_id, razorpay_signature, services} = req.body
+
+        const body = razorpay_order_id + "|" + razorpay_payment_id
+        const expectedSignature = crypto.createHmac("sha256",process.env.RAZORPAY_SECRET_KEY).update(body).digest("hex")
+        const isAuthentic = expectedSignature === razorpay_signature
+
+        if(isAuthentic){
+            const payment = await Payment.create({
+                razorpay_order_id,
+                razorpay_payment_id, 
+                razorpay_signature
+            })
+            await Service.create({
+                ...services,paidat:new Date(Date.now()),
+                paymentinfo:payment._id
+            })
+
+            res.status(201).json({
+                success:true,
+                message:`order placed successfully Payment ID ${payment._id}`
+            })
+
+        }else{
+            return next(res.status(404).json({
+                success:false,
+                message:"payment failed"
+            }))
+        }
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
 
 export const allServices = async (req, res) => {
     try {
